6b225150781fc7dc6dfb4d04386c3e6d1d2341bc
vladproduction <vladbogdantsev@gmail.com>
2023-11-20 13:59:04 +0200

    WatchRunner add

---
+++ WatchRunner/src/main/java/org01/Main.java
@@ -0,0 +1,8 @@
+package org01;
+
+public class Main {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        //StopWatch stopWatch2 = new StopWatch();
+    }
+}
