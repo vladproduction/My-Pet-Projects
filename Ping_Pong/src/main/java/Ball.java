44f8c37793176e024ae12067db7733d42de5df7d
vladproduction <vladbogdantsev@gmail.com>
2023-11-20 14:49:39 +0200

    Ping_Pong add

---
+++ Ping_Pong/src/main/java/Ball.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+
+    Random random;
+    int xVelocity; //how fast ball moving by x
+    int yVelocity; //how fast ball moving by y
+    int initialSpeed = 2;
+
+    public Ball(int x, int y, int width, int height) {
+        super(x,y,width,height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if(randomXDirection==0)
+            randomXDirection--;
+        setXDirection(randomXDirection*initialSpeed);
+
+        int randomYDirection = random.nextInt(2);
+        if(randomYDirection==0)
+            randomYDirection--;
+        setYDirection(randomYDirection*initialSpeed);
+    }
+
+    public void setXDirection(int randomXDirection){
+        xVelocity = randomXDirection;
+    }
+    public void setYDirection(int randomYDirection){
+        yVelocity = randomYDirection;
+    }
+    public void move(){
+        x += xVelocity;
+        y += yVelocity;
+
+    }
+    public void draw(Graphics g){
+        g.setColor(Color.pink);
+        g.fillOval(x,y,height,width);
+    }
+}
---
+++ Ping_Pong/src/main/java/Starter_PingPongGame.java
@@ -0,0 +1,7 @@
+public class Starter_PingPongGame {
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+    }
+}
